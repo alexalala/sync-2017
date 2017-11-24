e852cc2526143263780bd8dae18b106630a95516
Alexandra Moore <alexmoore94@hotmail.co.uk>
2017-11-24 13:50:50 +0000

    black search styled - needs icon

--- src/search.js
+++ src/search.js
@@ -3,10 +3,11 @@ import './search.css';
 
 export default class Search extends Component {
   render () {
-    const placeholder = "Search";
+    const placeholder = "Search for items... (eg. black hoodie)";
     return (
       <div className="expanded_search">
         <input type="text" placeholder={placeholder} />
+        <button type="submit">Search</button>
       </div>
     );
   }
